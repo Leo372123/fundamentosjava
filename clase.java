20d8ce446420d3fbe4edc1f800400c0d81f92110
Leo372123 <gustv4400@hotmail.com>
2025-06-05 10:31:28 +0200

    first commit

---
+++ clase.java
@@ -0,0 +1,9 @@
+public class clase {
+
+    public static void main(String[] args) {
+        System.out.println("tengo sueño");
+System.out.println("ya no tengo sueño");
+System.out.println("otra commit");
+
+    }
+}
