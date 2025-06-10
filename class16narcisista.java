092312ac47b86071e775870e2998861983f229fa
Leo372123 <gustv4400@hotmail.com>
2025-06-10 10:42:51 +0200

    class17

--- class16narcisista.java
+++ class16narcisista.java
@@ -1,5 +1,26 @@
-public class class16narcisista {
-    
+import java.util.Scanner;
+
+
+public class Class16Narcisista {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca un número narcisista");
+        String textoNumero = teclado.nextLine();
+        int longitud = textoNumero.length();
+        int suma = 0;
+        for (int i = 0; i < textoNumero.length(); i++){
+            char letra = textoNumero.charAt(i);
+            String aux = String.valueOf(letra);
+            int numero = Integer.parseInt(aux);
+            int operacion = (int) Math.pow(numero, longitud);
+            suma += operacion;
+        }
+        if (suma == Integer.parseInt(textoNumero)){
+            System.out.println("El número " + textoNumero + " es narcisista");
+        }else{
+            System.out.println("El número NO es narcisista");
+        }
+    }
 }
 // nesesitamos un programa para mostrar si un numero es narcisista o no lo es 
 // un numero narcisista es aquel que la suma de la potencia de su longitud es el 
